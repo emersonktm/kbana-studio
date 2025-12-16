37d7dabc0439ab9c402b2b2ff20ac6d2ab8060df
emersonktm <114255428+emersonktm@users.noreply.github.com>
2025-12-16 10:10:14 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/kbana-studio/",
+  base: "/", // 👈 MUITO IMPORTANTE
   server: {
     host: "::",
     port: 8080,
